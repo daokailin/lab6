f28b3be5be3bf028345aa33671d8e8053232f0ad
Daokai Lin <dalin@ucsd.edu>
2020-02-13 00:10:12 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,29 +2,45 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+    $('.project a').click(addProjectDetails);
 
-	// $('#colorBtn').click(randomizeColors);
+    // $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+    $.get("/project/" + idNumber, callBackFn);
+
+
+    console.log("User clicked on project " + idNumber);
+}
+
+function callBackFn(result) {
+    console.log(result);
+
+    console.log('#' + result['id'] + ' .thumbnail .details');
+    var projectHTML = '<p>' + result['title'] + '</p>' +
+        '<p><small>' + result['date'] + '</small></p>' +
+        '<img class="detailsImage" src=' + result['image'] + '>' +
+        '<p>' + result['summary'] + '</p>';
+    $('#project' + result['id'] + ' .thumbnail .details').html(projectHTML);
+
+
 }
